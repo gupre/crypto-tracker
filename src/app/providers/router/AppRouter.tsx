af1b56b54b9d6adadafeb12434d48f84298d7f4e
Fedorova Anastasia <ms.anastasiy2003@mail.ru>
2026-05-14 13:08:32 +0300

    feat: add deploy

--- src/app/providers/router/AppRouter.tsx
+++ src/app/providers/router/AppRouter.tsx
@@ -4,14 +4,17 @@ import { CoinsListPage } from '@/pages/coins-list'
 import { CoinDetailPage } from '@/pages/coin-detail'
 import { WatchlistPage } from '@/pages/watchlist'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <CoinsListPage /> },
-      { path: 'coin/:id', element: <CoinDetailPage /> },
-      { path: 'watchlist', element: <WatchlistPage /> },
-    ],
-  },
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <CoinsListPage /> },
+        { path: 'coin/:id', element: <CoinDetailPage /> },
+        { path: 'watchlist', element: <WatchlistPage /> },
+      ],
+    },
+  ],
+  { basename: '/crypto-tracker' }
+)
